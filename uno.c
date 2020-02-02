fe0cf012cd092f8ad6d5ec4572586585f10d046e
DeerisLeGris <yoshi_gris@outlook.com>
2020-02-02 21:59:17 +0100

    Update uno.c

--- uno.c
+++ uno.c
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 
 void jouer();
-void creerLeDeck(Carte* deck, int* taille);
 void afficherLeDeck(Carte* deck, int taille);
+void afficherCarte(Carte carte) ;
 
 int main() {
     generatePile();
 
-    Players P = addPlayer(NULL); //J1
+    /*Players P = addPlayer(NULL); //J1
     for (int i = 0; i < 2; i++) //crée 4 joueurs
         P = addPlayer(P);
     
@@ -21,68 +21,72 @@ int main() {
     Player p1 = getPlayer(P, 0);
     takeCard(p1);
 
-    showPlayers(P);
+    showPlayers(P);*/
 
-    //jouer();
+    jouer();
 }
 
 void jouer() {
-    int joueurActuel = 1; //1 = Joueur 1 et 2 = Joeuur 2
-    Carte cartesUn[109], cartesDeux[109]; //Le deck des deux joueurs
-    int cartesUnTaille = 0, cartesDeuxTaille = 0; //La nombres de cartes dans chaque deck
-    Carte* carteActuelle; //La carte dernièrement posé
+    Players P;
+    int nmbJoueurs = 0;
+    do {
+        printf("Bienvenue sur UNO ! Combien de joueurs êtes vous : ");
 
-    creerLeDeck(cartesUn, &cartesUnTaille);
-    creerLeDeck(cartesDeux, &cartesDeuxTaille);
+        scanf("%d", &nmbJoueurs);
+    } while(nmbJoueurs <= 0);
+
+    for (int i = 0; i < nmbJoueurs; i++) //crée 4 joueurs
+        P = addPlayer(P);
+
+    int joueurActuel = 0; //ID du joueur actuel
+    int sensNormal = 1; //Sens du jeu
+    Carte carteActuelle = pop(); //La carte dernièrement posé
+    Player tempPlayer;
 
     while(1) {
         printf("C'est au joueur %d de jouer !\n", joueurActuel);
+        tempPlayer = getPlayer(P, joueurActuel);
 
-        int choix = 0, tailleActuelle;
+        int choix = 0, tailleActuelle = tempPlayer->totalCard;
 
-        if(joueurActuel == 1) {
-            tailleActuelle = cartesUnTaille;
-            afficherLeDeck(cartesUn, tailleActuelle);
-        } else {
-            tailleActuelle = cartesDeuxTaille;
-            afficherLeDeck(cartesDeux, tailleActuelle);
-        }
+        afficherLeDeck(tempPlayer->cartes, tailleActuelle);
+
+        printf("\n\nDernière carte jouer : ");
+        afficherCarte(carteActuelle);
 
         //Choix de la carte ou alors piocher une carte
         do {
-            printf("\n\nEcrire -1 pour piocher une carte\n");
+            printf("\nEcrire -1 pour piocher une carte\n");
             printf("Ou alors selectionner une carte de 0 à %d en écrivant son numéro\n\nChoix : ", tailleActuelle - 1);
 
             scanf("%d", &choix);
         } while(choix <= -1 && choix >= tailleActuelle);
 
         if(choix == -1) {
-            //Piocher une carte
-            if(joueurActuel == 1) {
-                cartesUn[cartesUnTaille++] = pop();
-            } else {
-                cartesDeux[cartesDeuxTaille++] = pop();
-            }
+            takeCard(tempPlayer);
         } else {
             //Jouer une carte
         }
 
-        //Changement de joueur pour le tour suivant
-        if(joueurActuel == 1)
-            joueurActuel = 2;
-        else
-            joueurActuel = 1;
-
         //Voir s'il y a un des deux joueurs qui a gagné
-        if(cartesUnTaille == 0) {
-            printf("Bravo joueur 1, tu as gagné !\n");
+        if(tempPlayer->totalCard == 0) {
+            printf("Bravo joueur %d, tu as gagné !\n", joueurActuel);
             break;
         }
 
-        if(cartesDeuxTaille == 0) {
-            printf("Bravo joueur 2, tu as gagné !\n");
-            break;
+        //Changement de joueur pour le tour suivant
+        if(sensNormal) {
+            if(joueurActuel + 1 >= nmbJoueurs)
+                joueurActuel = 0;
+            else
+                joueurActuel++;
+        } else {
+            if(joueurActuel - 1 <= 0)
+                joueurActuel = nmbJoueurs - 1;
+            else
+                joueurActuel--;
         }
+        
     }
 }
 
@@ -108,14 +112,79 @@ void afficherLeDeck(Carte* deck, int taille) {
             break;
         }
 
-        printf("\n(%d) %d - %s", i, deck[i].num, couleur);
+        switch(deck[i].num) {
+            case 10:
+            printf("\n(%d) Carte passe ton tour - %s", i, couleur);
+            break;
+
+            case 11:
+            printf("\n(%d) Carte changement de sens - %s", i, couleur);
+            break;
+
+            case 12:
+            printf("\n(%d) Carte +2 - %s", i, couleur);
+            break;
+
+            case 13:
+            printf("\n(%d) Carte changement de couleur", i);
+            break;
+
+            case 14:
+            printf("\n(%d) Carte +4 - %s", i, couleur);
+            break;
+
+            default:
+            printf("\n(%d) %d - %s", i, deck[i].num, couleur);
+            break;
+        }
+        
     }
 }
 
-void creerLeDeck(Carte* deck, int* taille) {
-    for(int i = 0; i < 7; i++) {
-        deck[i] = pop();
-    }
+void afficherCarte(Carte carte) {
+    char* couleur;
 
-    *taille = 7;
+        switch(carte.color) {
+            case B:
+            couleur = "Bleu";
+            break;
+
+            case R:
+            couleur = "Rouge";
+            break;
+
+            case G:
+            couleur = "Vert";
+            break;
+
+            case Y:
+            couleur = "Jaune";
+            break;
+        }
+
+        switch(carte.num) {
+            case 10:
+            printf("Carte passe ton tour - %s\n", couleur);
+            break;
+
+            case 11:
+            printf("Carte changement de sens - %s\n", couleur);
+            break;
+
+            case 12:
+            printf("Carte +2 - %s\n", couleur);
+            break;
+
+            case 13:
+            printf("Carte changement de couleur\n");
+            break;
+
+            case 14:
+            printf("Carte +4 - %s\n", couleur);
+            break;
+
+            default:
+            printf("%d - %s\n", carte.num, couleur);
+            break;
+        }
 }
