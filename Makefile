f2e08516e0be1451f0e64135899367a35feee8ad
delvict <45336586+delvict@users.noreply.github.com>
2020-05-02 22:05:19 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
 CC = gcc
 SRC = rules.c pile.c controls.c uno.c
 OBJ = $(SRC:.c=.o)
-SRC_B = rules.c pile.c controls.c tests_unitaire.c
-OBJ_B = $(SRC_B:.c=.o)
-HEADERS = rules.h pile.h controls.h
+HEADERS = rules.h pile.h controls.h uno.h
 CFLAGS = -Wall
 PROG = UNO
 
@@ -11,5 +9,3 @@ $(PROG): $(OBJ) $(HEADERS)
 	$(CC) -o $@ $(OBJ)
 %.o: %.c
 	$(CC) -c $< $(CFLAGS)
-test: $(OBJ_B) $(HEADERS)
-	$(CC) -o $@ $(OBJ_B)
---
+++ uno.h
@@ -0,0 +1,29 @@
+#ifndef _UNO_H
+#define _UNO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// Fonction principale de jeu, prend en paramètre le nombre de joueurs.
+void jouer(int nbJoueurs);
+/// Prend en paramètre une liste de cartes ainsi que sa taille et l'affiche.
+void afficherLeDeck(Carte* deck, int taille);
+/// Prend une carte en paramètre et l'affiche.
+void afficherCarte(Carte carte);
+/// Menu du jeu qui demande le nombre de joueurs et lance la fonction jouer.
+void menu();
+/// Fonction qui remplit le tableau entrée en parametre de "-1" pour dire qu'il est vide.
+void remplirTab(int * tab, int taille);
+/// A supprimer ?
+void afficherTab(int * tab, int taille);
+/// Fonction qui return 1 si l'id entrée en parametre apartient au tableau entrée en parametre sinon return 0.
+int appartientTab(int * tab, int taille, int id);
+/// Prend en paramètre le joueur qui a joué, le sens de jeu et le nombre de joueurs. Renvoie l'id du prochain joueur on fonction du sens.
+int tour(int joueurActuel,int sensNormal, int nbJoueurs);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
