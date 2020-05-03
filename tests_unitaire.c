ade87130aadb875042e9b3e7db0984be1a58ac88
DeerisLeGris <yoshi_gris@outlook.com>
2020-05-03 20:36:15 +0200

    Ajout des assert

--- tests_unitaire.c
+++ tests_unitaire.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 
 int main() {
     printf("== TESTS UNITAIRE==\n\n\n=> Fichier controls.c\n\n");
@@ -14,45 +15,54 @@ int main() {
     *   => Controls.c
     */
 
-    printf("1) Génération d'une pile de cartes avec generatePile()\n");
+    printf("1) Génération d'une pile de cartes avec generatePile() et vérification de la pile avec empty()\n");
     generatePile();
+    assert(empty() == 0);
     printf("-- OK --\n\n");
 
     printf("2) Création d'une structure Players et création d'un nouveau joueur avec addPlayer()\n");
     Players P = NULL;
     P = addPlayer(P);
+    assert(P != NULL);
     printf("-- OK --\n\n");
 
     printf("3) Cherche le premier joueur dans la structure Players avec getPlayer(), ajout d'une carte au joueur avec takeCard() et montre les joeurs de la structure avec showPlayers() \n");
     printf("-> getplayer()\n");
     Player player = getPlayer(P, 0);
+    assert(player != NULL);
     printf("-> AVANT (ajout d'une carte)\n");
     showPlayers(P);
     printf("-> APRES (ajout d'une carte)\n");
     takeCard(player);
     showPlayers(P);
+    assert(player->totalCard == 8);
     printf("-- OK --\n\n");
     
     printf("4) Selection avec getPlayerFromPosition(), suppresion de la première carte du deck avec deleteCard() et montre les joeurs de la structure avec showPlayers()\n");
     printf("-> getPlayerFromPosition() -> première position\n");
     player = getPlayerFromPosition(P, 0);
+    assert(player != NULL);
     printf("-> AVANT (suppresion de la carte)\n");
     showPlayers(P);
     printf("-> APRES (suppresion de la carte)\n");
     deleteCard(player, player->cartes->num);
     showPlayers(P);
+    assert(player->totalCard == 7);
     printf("-- OK --\n\n");
 
     printf("5) Ajout d'un joueur avec addPlayer(), selection avec getPlayer(), suppresion d'un joueur avec removePlayer() et montre les joeurs de la structure avec showPlayers()\n");
     printf("-> addPlayer()\n");
     P = addPlayer(P);
+    assert(P->next != NULL);
     printf("-> getplayer()\n");
     player = getPlayer(P, 0);
+    assert(player != NULL);
     printf("-> AVANT (suppresion du joueur)\n");
     showPlayers(P);
     printf("-> APRES (suppresion du joueur)\n");
     P = removePlayer(P, player);
     showPlayers(P);
+    assert(P->next == NULL);
     printf("-- OK --\n\n");
 
     /*
