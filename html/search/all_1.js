c6318a5ff604ede7b886fc8e497bfa4d211493af
delvict <45336586+delvict@users.noreply.github.com>
2020-05-03 15:39:58 +0200

    Optimisation afficherLeDeck

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['addplayer',['addPlayer',['../controls_8c.html#a931821afa7bfadab717aab046220fe73',1,'addPlayer(Players P):&#160;controls.c'],['../controls_8h.html#a931821afa7bfadab717aab046220fe73',1,'addPlayer(Players P):&#160;controls.c']]],
   ['affichercarte',['afficherCarte',['../uno_8c.html#a59128fbf89ad2d2a828288bc27988817',1,'afficherCarte(Carte carte):&#160;uno.c'],['../uno_8h.html#a59128fbf89ad2d2a828288bc27988817',1,'afficherCarte(Carte carte):&#160;uno.c']]],
-  ['afficherledeck',['afficherLeDeck',['../uno_8c.html#add5e278f3823a648bc96f80a7fd8a49b',1,'afficherLeDeck(Carte *deck, int taille):&#160;uno.c'],['../uno_8h.html#a3072c6365f956771f9cf5101d28df249',1,'afficherLeDeck(Player P, int taile):&#160;uno.h']]],
+  ['afficherledeck',['afficherLeDeck',['../uno_8c.html#aec2f1c5aae2014dede1b9ddea64c7fe2',1,'afficherLeDeck(Player P):&#160;uno.c'],['../uno_8h.html#aec2f1c5aae2014dede1b9ddea64c7fe2',1,'afficherLeDeck(Player P):&#160;uno.c']]],
   ['affichertab',['afficherTab',['../uno_8h.html#a21b9be68bfc28555ef22334844a4c800',1,'uno.h']]],
   ['appartienttab',['appartientTab',['../uno_8c.html#a9933e93875680d03715ca4c7c098ea4b',1,'appartientTab(int *tab, int taille, int id):&#160;uno.c'],['../uno_8h.html#a9933e93875680d03715ca4c7c098ea4b',1,'appartientTab(int *tab, int taille, int id):&#160;uno.c']]],
   ['applyeffect',['applyEffect',['../n__uno_8c.html#a675b74b790b856a029a7030dc83e44a9',1,'n_uno.c']]]
--- uno.c
+++ uno.c
@@ -100,7 +100,7 @@ void jouer(int nbJoueurs) {
             malusPlus = 0;
         } else {
             printf("C'est au joueur %d de jouer !\n", tempPlayer->id);
-            afficherLeDeck(tempPlayer->cartes, tailleActuelle);
+            afficherLeDeck(tempPlayer);
             printf("\n\nDerniere carte jouer : ");
             afficherCarte(carteActuelle);
         }
@@ -217,57 +217,12 @@ void jouer(int nbJoueurs) {
     free(winTab);
 }
 
-/** On parcourt le tableau de cartes et pour chaque carte, on commence par trouver sa couleur, puis son numéro et on affiche ainsi la carte.
+/** On parcourt le tableau de cartes et on affiche chaque carte.
 */
-void afficherLeDeck(Carte* deck, int taille) {
-    for(int i = 0; i < taille; i++) {
-        char* couleur;
-
-        switch(deck[i].color) {
-            case B:
-            couleur = "Bleu";
-            break;
-
-            case R:
-            couleur = "Rouge";
-            break;
-
-            case G:
-            couleur = "Vert";
-            break;
-
-            case Y:
-            couleur = "Jaune";
-            break;
-        }
-
-        switch(deck[i].num) {
-            case 10:
-            printf("\n(%d) Carte passe ton tour - %s", i, couleur);
-            break;
-
-            case 11:
-            printf("\n(%d) Carte changement de sens - %s", i, couleur);
-            break;
-
-            case 12:
-            printf("\n(%d) Carte +2 - %s", i, couleur);
-            break;
-
-            case 13:
-            printf("\n(%d) Carte changement de couleur", i);
-            break;
-
-            case 14:
-            printf("\n(%d) Carte +4", i);
-            break;
-
-            default:
-            printf("\n(%d) %d - %s", i, deck[i].num, couleur);
-            break;
-        }
-        
-    }
+void afficherLeDeck(Player P) 
+{
+    for(int i = 0; i < P->totalCard; i++)
+        afficherCarte(P->cartes[i]);
 }
 
 /** Comme pour afficherLeDeck, on récupère la couleur et le numéro de la carte afin d'afficher la bonne carte.
