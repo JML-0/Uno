1426adcb37e04a134eb3137c2e62e18f538c4419
delvict <45336586+delvict@users.noreply.github.com>
2020-05-03 10:30:04 +0200

    Mise à jour commentaires

--- rules.h
+++ rules.h
@@ -1,8 +1,10 @@
 #ifndef _RULES_H
 #define _RULES_H
 
-#define NBCARTES 7    /* Nb de carte par joueur */
-#define PILE_MAX 108  /* Nb max de carte */
+/// Nombre de cartes par joueur 
+#define NBCARTES 7  
+/// Nombre maximum de cartes  
+#define PILE_MAX 108 
 
 #endif
 
